c1f6c9c6c476a1f7bf349c4d7edbaf31bab9f2ac
moteesh <moteesh@gmail.com>
2021-06-23 14:34:24 +0530

    input for keep ticking

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def initialize_world():
     user_friendly_world[14][13] = "A"
     world[14][14] = 1
     user_friendly_world[14][14] = "A"
-    print_world()
+    print_world(0)
 
 # populate world with random cells
 def initialize_world_random(size):
@@ -44,10 +44,10 @@ def initialize_world_random(size):
         for y in range(0, size):
             row.append(randint(0, 100) % 2)
         world.append(row)
-    print_world()
+    print_world(0)
 
 
-def print_world():
+def print_world(generation):
     global world, user_friendly_world
     user_friendly_world = []
     for x in range(0, len(world)):
@@ -59,7 +59,7 @@ def print_world():
             else:
               row.append("D")
         user_friendly_world.append(row)
-    print("User friendly world:")
+    print("User friendly world: Generation =", generation)
     for x in range(0, len(user_friendly_world)):
         print(user_friendly_world[x])
     
@@ -72,10 +72,17 @@ def populate_world():
 def tick():
     global generation_number
     generation_number += 1
-    print("ticked, generation number = ", generation_number)
     populate_world()
-
+    print("generation number = ", generation_number)
+    print_world(generation_number)
 
 #initialize_world_random(5)
 initialize_world()
-tick()
+keep_ticking = True
+while(keep_ticking):
+    tick()
+    answer = input("Keep ticking (Y/n) :")
+    if answer.lower().startswith("n"):
+        keep_ticking = False
+
+
