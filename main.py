f66807d7c2670b834594440c714bace8ad6c1590
moteesh <moteesh@gmail.com>
2021-06-23 15:26:46 +0530

    populte world

--- main.py
+++ main.py
@@ -4,51 +4,45 @@ from random import randint
 
 generation_number = 0
 world = None
-user_friendly_world = None
+world_size = 25
+
+def empty_world(world_size):
+    w = []
+    for x in range(0, world_size):
+        row = []
+        for y in range(0, world_size):
+            row.append(0)
+        w.append(row)
+
+    return w
 
 # populate world with random cells
 def initialize_world():
-    global world, user_friendly_world
-    size = 25
-    world = []
-    user_friendly_world = []
-    seed(100)
-    for x in range(0, size):
-        row = []
-        user_friendly_row = []
-        for y in range(0, size):
-            row.append(0)
-            user_friendly_row.append("D")
-        world.append(row)
-        user_friendly_world.append(user_friendly_row)
+    global world
+    world = empty_world(world_size)
     # initial values
     world[12][13] = 1
-    user_friendly_world[12][13] = "A"
     world[13][14] = 1
-    user_friendly_world[13][14] = "A"
     world[14][12] = 1
-    user_friendly_world[14][12] = "A"
     world[14][13] = 1
-    user_friendly_world[14][13] = "A"
     world[14][14] = 1
-    user_friendly_world[14][14] = "A"
     print_world(0)
 
 # populate world with random cells
-def initialize_world_random(size):
+def initialize_world_random():
     global world
     world = []
     seed(100)
-    for x in range(0, size):
+    for x in range(0, world_size):
         row = []
-        for y in range(0, size):
+        for y in range(0, world_size):
             row.append(randint(0, 100) % 2)
         world.append(row)
     print_world(0)
 
 
 def print_world(generation):
-    global world, user_friendly_world
+    global world
     user_friendly_world = []
     for x in range(0, len(world)):
         print(world[x])
@@ -64,15 +58,78 @@ def print_world(generation):
         print(user_friendly_world[x])
     
 
-def populate_world():
+def get_live_neighbours_count(x, y):
+    global world
+    count = 0
+    # left and right squares
+    if world[x][y-1]:
+        count += 1
+    if world[x][y+1]:
+        count += 1
+
+    # top and bottom squares
+    if world[x-1][y]:
+        count += 1
+    if world[x+1][y]:
+        count += 1
+
+    # top left square
+    if world[x-1][y-1]:
+        count += 1
+
+    # top right square
+    if world[x-1][y+1]:
+        count += 1
+
+    # bottom left square
+    if world[x+1][y-1]:
+        count += 1
+
+    # bottom right square
+    if world[x+1][y+1]:
+        count += 1
+
+    return count
+
+def populate_next_gen_world():
+    global world
     x, y = 0, 0
     print("x =", x, "y =", y)
-    
+    next_gen_world = empty_world(world_size)
+    for x in range(0, len(world)):
+        for y in range(0, len(world[x])):
+            live_neighbours_count = get_live_neighbours_count(x, y)
+
+            # live cell
+            if world[x][y]:
+                # 1. Any live cell with fewer than two live neighbors dies as if caused by underpopulation
+                if live_neighbours_count < 2:
+                    # the cell dies
+                    next_gen_world[x][y] = 0
+
+                # 2. Any live cell with two or three live neighbors lives on to the next generation.
+                elif live_neighbours_count == 2 or live_neighbours_count == 3:
+                    # the cell lives
+                    next_gen_world[x][y] = 1
+
+                # 3. Any live cell with more than three live neighbors dies, as if by overcrowding.
+                elif live_neighbours_count > 3:
+                    # the cell dies
+                    next_gen_world[x][y] = 0
+            # dead cell
+            else:
+                # 4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+               if live_neighbours_count == 3:
+                    # the cell lives
+                    next_gen_world[x][y] = 1
+
+    return next_gen_world
+
 
 def tick():
-    global generation_number
+    global world, generation_number
     generation_number += 1
-    populate_world()
+    world = populate_next_gen_world()
     print("generation number = ", generation_number)
     print_world(generation_number)
 
